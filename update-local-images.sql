81a373ee431e8f7254a1dc7a1fa173bde16fe2cd
bastiaanbeemsterboer <bastiaanbeemsterboer@gmail.com>
2025-08-31 07:23:28 +0200

    Remove temporary test files and fix security vulnerabilities - remove exposed Supabase service key

--- update-local-images.sql
+++
@@ -1,7 +0,0 @@
--- Update featured images for all blog posts with the local Lu Minous logo
-UPDATE blog_posts 
-SET featured_image = '/images/lu-minous-logo.jpg'
-WHERE status = 'published';
-
--- Verify the updates
-SELECT id, title, featured_image FROM blog_posts WHERE status = 'published';
